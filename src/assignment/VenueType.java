dd50a35af30a3ad14f2c378b0713b680240ade25
Jason <Jason@SILVERLINE>
2023-09-24 01:20:33 +0800

    9/23

---
+++ src/assignment/VenueType.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package assignment;
+
+/**
+ *
+ * @author Jason
+ */
+public enum VenueType {
+    Pavilion(500),
+    Midvalley(350),
+    Genting(600);
+    final int price;
+
+    VenueType(int price) {
+        this.price = price;
+    }
+
+}
