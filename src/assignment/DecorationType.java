54eca87e24d1cadffc90aae0bd6bde2a8110b482
Jason <Jason@SILVERLINE>
2023-09-24 01:06:54 +0800

    9/23

---
+++ src/assignment/DecorationType.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package assignment;
+
+/**
+ *
+ * @author Jason
+ */
+public enum DecorationType {
+    Luxury(9888),
+    Ordinary(5888);
+
+    final int price;
+
+    DecorationType(int price) {
+        this.price = price;
+    }
+}
